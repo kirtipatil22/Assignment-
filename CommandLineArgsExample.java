691ca95148bf8630a1c86a5839312c28ff5b1fd3
kirtipatil22 <121376697+kirtipatil22@users.noreply.github.com>
2024-03-23 17:26:09 +0530

    Add files via upload

---
+++ CommandLineArgsExample.java
@@ -0,0 +1,12 @@
+public class CommandLineArgsExample
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Below are arguments pass to program");
+		for(String a:args)
+		{
+			System.out.println(a);
+
+		}
+	}
+}
