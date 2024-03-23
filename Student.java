3322874af77dc37ec25ea8c8631aba94d7391274
kirtipatil22 <121376697+kirtipatil22@users.noreply.github.com>
2024-03-23 17:26:25 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+package school;
+public class Student
+{
+	public String name;
+	public String dob;
+	public int rollno;
+	public void show()
+	{
+		System.out.println("Name="+this.name);
+		System.out.println("DOB="+this.dob);
+		System.out.println("RollNumber="+this.rollno);
+	}
+}
+
+//javac -d . Student.java
+//java Student
