c337167410e2042f9d36f834fa96ab92ef33a980
kirtipatil22 <121376697+kirtipatil22@users.noreply.github.com>
2024-03-23 17:27:35 +0530

    Add files via upload

---
+++ StudentManager.java
@@ -0,0 +1,12 @@
+import school.Student;
+public class StudentManager
+{
+	public static void main(String[] args)
+	{
+		Student s = new Student();
+		s.name ="Akshita";
+		s.dob ="21/01/1999";
+		s.rollno=35;
+		s.show();
+	}
+}
